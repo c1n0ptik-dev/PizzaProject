7cef3fe2229c62ad68dd5b88b9f23eedfc41787c
avdebor <you@example.com>
2024-10-29 15:19:50 +0100

    Rebuilded the form of pizza order

--- Server/app.py
+++ Server/app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, request
 
-
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def main():
     return render_template('website/index.html')
 
-
 @app.route('/cashier', methods=['GET'])
 def cashier():
     return render_template('cashier-kitchen/cashier.html')
 
-
 @app.route('/send_data', methods=['POST'])
 def send_data():
-    pass
+    # Retrieve data from the form
+    pizza = request.form.get('pizza')
+    additional_info = request.form.get('additional_info')
+    toppings = request.form.get('toppings')
 
+    # Print the data to the console
+    print(f"Pizza: {pizza}, Additional Info: {additional_info}, Toppings: {toppings}")
+
+    return "Data received!"  # Response for form submission
 
 if __name__ == "__main__":
     app.debug = True
