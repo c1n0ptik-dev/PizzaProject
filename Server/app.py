55e4037ca3b3e5c1bac3ac55adf7733035e650d8
Darii <i553730@fontysict.nl>
2024-10-31 13:24:18 +0100

    some changes to app.py

--- Server/app.py
+++ Server/app.py
@@ -83,7 +83,6 @@ def menu():
 
 @app.route('/basket', methods=['GET', 'POST'])
 def basket():
-<<<<<<< HEAD
     if 'delete' in request.form:
         delete_button = request.form.get('delete')
         if delete_button == 'pressed':
@@ -92,33 +91,23 @@ def basket():
             cursor = conn.cursor()
             cursor.execute("DELETE FROM Basket WHERE Id=?", (pizzaid,))
             conn.commit()
-=======
-    # if 'delete' in request.form:
-    #     delete_button = request.form.get('delete')
-    #     if delete_button == 'pressed':
-    #         conn = sqlite3.connect('database/database.db')
-    #         cursor = conn.cursor()
-    #         cursor.execute("DELETE FROM Basket WHERE Id=?", (orderid,))
-    #         conn.commit()
->>>>>>> 9702b32d0f13f119f4accf8fef5eca71628376f7
-
-    # items = get_data_from_db("Basket")
-    # return render_template("website/overview.html", data=items)
-    return render_template("website/overview.html")
+    items = get_data_from_db("Basket")
+    return render_template("website/overview.html", data=items)
 
 
 @app.route('/basket_data', methods=[''])
 def basket_data():
     pizzaType = request.form.get('pizzaType')
+    img = request.form.get('img')
     size = request.form.get('size')
     price = request.form.get('price')
 
     conn = sqlite3.connect('database/database.db')
     cursor = conn.cursor()
     cursor.execute('''
-        INSERT INTO Orders (OrderType, Size, Price)
+        INSERT INTO Orders (OrderType, Img, Size, Price)
         VALUES (?, ?, ?)
-    ''', (pizzaType, size, price))
+    ''', (pizzaType, img, size, price))
 
     conn.commit()
     conn.close()
